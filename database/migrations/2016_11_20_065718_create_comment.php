8df110a1f5ba1c6b4182e5d283eec43612d413b1
omjoko <heru.m175@gmail.com>
2017-12-13 03:59:28 +0700

    migrate

--- database/migrations/2016_11_20_065718_create_comment.php
+++ database/migrations/2016_11_20_065718_create_comment.php
@@ -15,16 +15,16 @@ class CreateComment extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('id_posts')->unsigned();
+            $table->integer('id_posts')->nullable()->unsigned();
             $table->foreign('id_posts')->references('id')->on('posts')->onUpdate('cascade')->onDelete('cascade');
-            $table->integer('id_pages')->unsigned();
+            $table->integer('id_pages')->nullable()->unsigned();
             $table->foreign('id_pages')->references('id')->on('pages')->onUpdate('cascade')->onDelete('cascade');
-            $table->string('nama');
+            $table->string('nama')->nullable();
             $table->string('foto')->nullable();
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->string('judul')->nullable();
-            $table->longText('deskripsi');
-            $table->boolean('read');
+            $table->longText('deskripsi')->nullable();
+            $table->boolean('read')->nullable();
             $table->integer('parent')->nullable()->unsigned();
             $table->foreign('parent')->references('id')->on('comments')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
